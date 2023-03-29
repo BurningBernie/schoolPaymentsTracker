234476ccf4af5aa33f2b16dde3c86d9d90d1bf59
Bernardo <bernardo_ruizesquide@hotmail.com>
2023-03-28 21:46:26 -0300

    more work on select tag

--- WebPage/functions.js
+++ WebPage/functions.js
@@ -1,7 +1,7 @@
 function selector(){
    let selectorInput = document.getElementById("SL")
 
-   selectorInput.options[0]= new Option ('Select a School to begin')
+   //creo una iteración nueva del Class Option en el array options
    selectorInput.options[1]= new Option ('School Example 01')
    selectorInput.options[2]= new Option ('School Example 02')
    selectorInput.options[3]= new Option ('School Example 03')
